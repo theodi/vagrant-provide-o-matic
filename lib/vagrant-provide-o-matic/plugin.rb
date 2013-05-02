ee2397b3d77884771cee4e94407e4dea9ba12af1
pikesley <github@orgraphone.org>
2013-05-02 16:13:27 +0100

    We don't need ALL THE HOOKS

--- lib/vagrant-provide-o-matic/plugin.rb
+++ lib/vagrant-provide-o-matic/plugin.rb
@@ -6,40 +6,33 @@ module Vagrant
       Allow multiple providers from the same Vagrantfile
       DESC
 
-      action_hook(:vagrant_provide_o_matic_derp, :machine_action_up) do |hook|
-        puts "*** up ***"
-        hook.after(::Vagrant::Action::Builtin::ConfigValidate, Vagrant::ProvideOMatic::Action.derp)
+      action_hook(:vagrant_provide_o_matic_up, :machine_action_up) do |hook|
+        hook.before(::Vagrant::Action::Builtin::ConfigValidate, Vagrant::ProvideOMatic::Action.before_up)
+        hook.after(::Vagrant::Action::Builtin::ConfigValidate, Vagrant::ProvideOMatic::Action.after_up)
       end
 
-      action_hook(:vagrant_provide_o_matic_derp, :machine_action_destroy) do |hook|
-        puts "*** destroy ***"
+      action_hook(:vagrant_provide_o_matic_destroy, :machine_action_destroy) do |hook|
+        hook.after(::Vagrant::Action::Builtin::ConfigValidate, Vagrant::ProvideOMatic::Action.after_destroy)
       end
 
-      action_hook(:vagrant_provide_o_matic_derp, :machine_action_halt) do |hook|
-        puts "*** halt ***"
+      action_hook(:vagrant_provide_o_matic_ssh, :machine_action_ssh) do |hook|
+        hook.before(::Vagrant::Action::Builtin::ConfigValidate, Vagrant::ProvideOMatic::Action.before_ssh)
       end
 
-      action_hook(:vagrant_provide_o_matic_derp, :machine_action_ssh) do |hook|
-        puts "*** ssh ***"
-      end
-
-      action_hook(:vagrant_provide_o_matic_derp, :machine_action_suspend) do |hook|
-        puts "*** suspend ***"
-      end
-
-      action_hook(:vagrant_provide_o_matic_derp, :machine_action_reload) do |hook|
-        puts "*** reload ***"
-        hook.after(::Vagrant::Action::Builtin::ConfigValidate, Vagrant::ProvideOMatic::Action.derp)
-      end
-
-      action_hook(:vagrant_provide_o_matic_derp, :machine_action_resume) do |hook|
-        puts "*** resume ***"
-      end
-
-      action_hook(:vagrant_provide_o_matic_derp, :machine_action_status) do |hook|
-        puts "*** status ***"
-        hook.after(::Vagrant::Action::Builtin::ConfigValidate, Vagrant::ProvideOMatic::Action.derp)
-      end
+#     action_hook(:vagrant_provide_o_matic_derp, :machine_action_halt) do |hook|
+#     end
+#
+#     action_hook(:vagrant_provide_o_matic_derp, :machine_action_suspend) do |hook|
+#     end
+#
+#     action_hook(:vagrant_provide_o_matic_derp, :machine_action_reload) do |hook|
+#     end
+#
+#     action_hook(:vagrant_provide_o_matic_derp, :machine_action_resume) do |hook|
+#     end
+#
+#     action_hook(:vagrant_provide_o_matic_derp, :machine_action_status) do |hook|
+#     end
 
       config("provide_o_matic") do
         Config
