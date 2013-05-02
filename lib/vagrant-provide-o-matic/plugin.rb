661a05f62f742ac8ac6f57406ec1b967b2f8a01a
pikesley <github@orgraphone.org>
2013-05-02 09:30:22 +0100

    ALL THE HOOKS

--- lib/vagrant-provide-o-matic/plugin.rb
+++ lib/vagrant-provide-o-matic/plugin.rb
@@ -8,7 +8,7 @@ module Vagrant
 
       action_hook(:vagrant_provide_o_matic_derp, :machine_action_up) do |hook|
         puts "*** up ***"
-#        hook.after(::Vagrant::Action::Builtin::ConfigValidate, Vagrant::ProvideOMatic::Action.derp)
+        hook.after(::Vagrant::Action::Builtin::ConfigValidate, Vagrant::ProvideOMatic::Action.derp)
       end
 
       action_hook(:vagrant_provide_o_matic_derp, :machine_action_destroy) do |hook|
@@ -29,10 +29,16 @@ module Vagrant
 
       action_hook(:vagrant_provide_o_matic_derp, :machine_action_reload) do |hook|
         puts "*** reload ***"
+        hook.after(::Vagrant::Action::Builtin::ConfigValidate, Vagrant::ProvideOMatic::Action.derp)
       end
 
       action_hook(:vagrant_provide_o_matic_derp, :machine_action_resume) do |hook|
         puts "*** resume ***"
+      end
+
+      action_hook(:vagrant_provide_o_matic_derp, :machine_action_status) do |hook|
+        puts "*** status ***"
+        hook.after(::Vagrant::Action::Builtin::ConfigValidate, Vagrant::ProvideOMatic::Action.derp)
       end
 
       config("provide_o_matic") do
